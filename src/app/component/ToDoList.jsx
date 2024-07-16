e89781c0ba84a922ac643d594f4102cac9daa320
TCFII <168501077+TCFII@users.noreply.github.com>
2024-07-16 00:49:42 +0000

    OK-got it working w-list

---
+++ src/app/component/ToDoList.jsx
@@ -0,0 +1,59 @@
+import React, {useState } from 'react';
+
+export function ToDoList ({ }) {
+
+const [todos,setTodos]=useState([]);
+const [task,setTask]=useState("");
+
+const addTask = () => {
+    if (task.trim() !=='') {
+        setTodos([...todos, task]);
+        setTask('');
+    }
+};
+
+const handleChangeTask = (ev) => {
+    setTask(ev.target.value);
+};
+
+const handleEnterKey = (ev) => {
+    if(ev.key === "Enter") {
+        addTask();
+    }
+};
+const handleDelete = (index) => {
+    const newTodos = todos.filter((todos,id) => id !== index);
+    setTodos(newTodos);
+};
+
+return (
+<div className="w-3/4 flex flex-col items-center border-2 rounded-lg border-black">
+    <h1 className="text-xl mt-4 mb-4">To Do List</h1>
+    <div>
+        <input  className="w-full border-2 rounded-lg border-black mb-4" 
+                placeholder="Enter new task!" 
+                value={task}
+                onChange={handleChangeTask}
+                onKeyDown={handleEnterKey}
+        />
+    </div>
+    <div className="w-1/2 border-2 rounded-lg border-gray mb-4">
+        <ul>
+            {todos.map((todos,index) => (
+                <li className="flex justify-between mr-2" key={index}>{todos}
+                <button class="ml-auto text-[10px] text-white hover:text-red-500 rounded"
+                    onClick={() => handleDelete (index)}>
+                    X
+                </button>
+                </li>
+            ))}
+        </ul>
+    </div>
+    <div className="w-1/2 border-2 rounded-lg border-transparent mb-4">
+       <p>To do items left:&nbsp;{todos.length}</p>
+    </div>
+
+
+</div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,14 @@
+"use client";
+
 import Link from "next/link";
+import {ToDoList} from "./component/ToDoList"
+
 
 export default function Home() {
 
     return (
         <div className="container mx-auto text-white text-xl">
-            Hello MDC-27
-            <p>
-                <Link className="text-sky-400 hover:underline" href="login">Login</Link>
-                <a href="/login" className="text-sky-800">Login</a>
-            </p>
-            <p>
-                <Link className="text-sky-400 hover:underline" href="about-us">About Us</Link>
-            </p>
+            <ToDoList />
         </div>
     );
 }
